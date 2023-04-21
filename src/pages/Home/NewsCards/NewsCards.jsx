a9513ad02920a456aaf860519c40f2b2bb6a4471
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-04-21 21:06:50 +0600

    progress on dragon news

--- src/pages/Home/NewsCards/NewsCards.jsx
+++ src/pages/Home/NewsCards/NewsCards.jsx
@@ -10,7 +10,7 @@ const NewsCards = ({ news }) => {
                 <Card.Header>Featured</Card.Header>
                 <Card.Body>
                     <Card.Title>{title}</Card.Title>
-                    <Card.Img variant="top" src={image_url} />
+                    <Card.Img variant="top" src={image_url && image_url} />
                     <Card.Text>
                         {details.slice(0, 200)}...<Link to={`/news/${_id}`}>Read More</Link>
                     </Card.Text>
