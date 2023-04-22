52c09caec6f3b64d9a22753d611cf85ea19a3ad8
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-04-23 00:34:52 +0600

    updated

---
+++ funTask.js
@@ -0,0 +1,4 @@
+console.log('Hello,Heros!! Eid Mubarak!!!') 
+console.error("Ehh! I haven't got any eid salami • !!") 
+console.warn("Jhankar vat, I'll do nothing without salami !")
+ console.table([{ name: 'boo vat', salami: 600 }, { name: "abbu", salami: 500 }]) 
