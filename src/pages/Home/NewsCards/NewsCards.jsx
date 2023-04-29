0768d0bbe179b343d32ada4ab44cd7e0cf5416e8
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-04-29 20:55:16 +0600

    card footer updated

--- src/pages/Home/NewsCards/NewsCards.jsx
+++ src/pages/Home/NewsCards/NewsCards.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
+import { FaEye } from 'react-icons/fa';
+
 
 const NewsCards = ({ news }) => {
     const { _id, rating, total_view, title, author, image_url, details } = news
+    console.log(rating)
     return (
         <>
             <Card className=" mb-4 ">
@@ -14,9 +19,17 @@ const NewsCards = ({ news }) => {
                     <Card.Text>
                         {details.slice(0, 200)}...<Link to={`/news/${_id}`}>Read More</Link>
                     </Card.Text>
-                  
+
                 </Card.Body>
-                <Card.Footer className="text-muted">2 days ago</Card.Footer>
+                <Card.Footer className="text-muted d-flex">
+                    <div className='d-flex flex-grow-1 justify-content-start align-items-center' >
+                        <Rating style={{ maxWidth: 180 }} value={(rating?.number)} readOnly />
+                        <span className='ms-2 fs-5' >{rating?.number}</span>
+                    </div>
+                    <div>
+                       <span className='fs-3'> <FaEye ></FaEye> </span>{total_view}
+                    </div>
+                </Card.Footer>
             </Card>
 
         </>
